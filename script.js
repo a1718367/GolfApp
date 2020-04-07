2577c0b6d9e7ece0cb09d6302b895af58473aabc
PzOng <pangszee.ong@gmail.com>
2020-04-07 12:53:31 +0930

    test2

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function success(pos){
   console.log(latn, lonn)
   info(latn,lonn);
   forcast(latn, lonn);
-  initMap()
+  initMap(latn, lonn);
 
   }
   
@@ -200,21 +200,8 @@ displaycompass()
 
 //Matt's - Map marking
 
-console.log("test");
 
-var lat = -34.962764;
-
-var log = 138.526134;
-
-// function initMap() {
-//     var Latlog = new google.maps.LatLng(lat, log);
-//     var map = new google.maps.Map(
-//         document.getElementById('map'), {zoom: 30, center: Latlog, mapTypeId: 'satellite'});
-//     var marker = new google.maps.Marker({position: Latlog, map: map});
-// }
-
-
-function initMap() {
+function initMap(lat, log) {
     var Latlog = new google.maps.LatLng(lat, log);
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 30, center: Latlog,
