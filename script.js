f9949aa8182a8c66945527193f7fa260f4c17a80
PzOng <pangszee.ong@gmail.com>
2020-04-08 18:33:57 +0930

    prototype map function

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ var displaycompass = function(){
     createButton.setAttribute("class","bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full")   
     createButton.setAttribute("id","but")
     createButton.setAttribute("type","button")       
-    document.getElementsByClassName("compassdisplay")[0].appendChild(createButton); 
+    //document.getElementsByClassName("compassdisplay")[0].appendChild(createButton); 
 
     /* https://stackoverflow.com/questions/16048514/can-i-use-javascript-to-get-the-compass-heading-for-ios-and-android
 stackoverflow was used to find the ollowing code to gain access to movement and orientation on ios devices*/
@@ -136,13 +136,9 @@ stackoverflow was used to find the ollowing code to gain access to movement and 
   }
 
 
-          
-
           var rmtext = document.getElementById('pleasenote')
           rmtext.innerHTML="";
           
-
-
   
      if (window.DeviceOrientationEvent) {
      window.addEventListener('deviceorientation', function(eventData) {
@@ -156,8 +152,6 @@ stackoverflow was used to find the ollowing code to gain access to movement and 
   var direc = 360 - compassdir 
   var arrowimg = document.getElementById('arrow');
   arrowimg.setAttribute('style','transform:rotate('+ direc +'deg)');
- 
-
   
   
     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=metric&appid='+apikey;
@@ -179,20 +173,12 @@ stackoverflow was used to find the ollowing code to gain access to movement and 
 
           },
           
-
 })
-
- 
 
 
     });
-    
-
-    
+        
   }
-
-
-
 
 }
 displaycompass()
@@ -204,7 +190,7 @@ displaycompass()
 function initMap(lat, log) {
     var Latlog = new google.maps.LatLng(lat, log);
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 30, center: Latlog,
+      zoom: 25, center: Latlog,
       mapTypeId: 'satellite'
     });
 
@@ -222,3 +208,116 @@ function initMap(lat, log) {
     });
     }
 };
+
+function renderCourseMap(userpos) {
+
+  var user_loc = {lat: -34.961265, lng: 138.527800};
+    // course_data array holds information on golf course, NOT ACTUAL CO-ORDINATE OF THE GOLF COURSE.
+    var course_data = {
+      hole_1: { 
+        cordinates : {lat: -34.961287, lng: 138.527916},
+        scoreboard : { par:4, black_tee:301,white_tee:301 }
+      },
+      hole_2: {
+        cordinates : {lat: -34.964530, lng: 138.527971},
+        scoreboard : { par:2, black_tee:418,white_tee:410 }
+      },
+      hole_3: { 
+        cordinates : {lat: -34.964244, lng: 138.524223},
+        scoreboard : { par:3, black_tee:202, white_tee:178 }
+      },
+      hole_4: {
+        cordinates : {lat: -34.960518, lng: 138.532194},
+        scoreboard : { par:4, black_tee:315, white_tee:296 }
+      },
+      hole_5: {
+        cordinates : {lat: -34.964791, lng: 138.521978},
+        scoreboard : { par:5, black_tee:490, white_tee:490 }
+      },
+      hole_6: {
+        cordinates : {lat: -34.961085, lng: 138.524329},
+        scoreboard : { par:4, black_tee:430, white_tee:400 }
+      },
+      hole_7: {
+        cordinates : {lat: -34.963582, lng: 138.526938},
+        scoreboard : { par:4, black_tee:394, white_tee:394 }
+      },
+      hole_8: {
+        cordinates : {lat: -34.961744, lng: 138.525301},
+        scoreboard : { par:4, black_tee:407, white_tee:384 }
+      },
+      hole_9: {
+        cordinates : {lat: -34.958662, lng: 138.527802},
+        scoreboard : { par:4, black_tee:376, white_tee:376 }
+      },
+      hole_10: {
+        cordinates : {lat: -34.961706, lng: 138.526528},
+        scoreboard : { par:4, black_tee:382, white_tee:366 }
+      },
+      hole_11: {
+        cordinates : {lat: -34.959448, lng: 138.528874},
+        scoreboard : { par:3, black_tee:168, white_tee:168 }
+      },
+      hole_12: {
+        cordinates : {lat: -34.961470, lng: 138.527165},
+        scoreboard : { par:5, black_tee:396, white_tee:349 }
+      },
+      hole_13: {
+        cordinates : {lat: -34.958724, lng: 138.531413},
+        scoreboard : { par:3, black_tee:164,white_tee:164 }
+      },
+      hole_14: {
+        cordinates : {lat: -34.961458, lng: 138.529532},
+        scoreboard : { par:3, black_tee:201,white_tee:211 }
+      },
+      hole_15: {
+        cordinates : {lat: -34.961545, lng: 138.532869},
+        scoreboard : { par:4, black_tee:321,white_tee:310 }
+      },
+      hole_16: {
+        cordinates : {lat: -34.958326, lng: 138.533929},
+        scoreboard : { par:3, black_tee:144,white_tee:144 }
+      },
+      hole_17: {
+        cordinates : {lat: -34.959692, lng: 138.533539},
+        scoreboard : { par:4, black_tee:386,white_tee:368 }
+      },
+      hole_18: {
+        cordinates : {lat: -34.961558, lng: 138.533067},
+        scoreboard : { par:5, black_tee:496,white_tee:496 }
+      }
+    }
+
+    var markers = []; 
+    var infowindow = [];
+
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 16.5, center: user_loc}); 
+
+    for (let i = 1; i < 18; i++) {
+    // draw holes / bind click events to flags to infowindo/   
+        infowindow[i] = new google.maps.InfoWindow({
+              content: `<strong style="color:green">Hole - ${i}</strong>
+              <br>Par: ${ eval('course_data.hole_'+i+'.scoreboard.par')}
+              <br>Black Tee: ${ eval('course_data.hole_'+i+'.scoreboard.black_tee')}
+              <br>White Tee: ${ eval('course_data.hole_'+i+'.scoreboard.white_tee')}
+              <br>Coordinates: ${ eval('course_data.hole_'+i+'.cordinates.lat')} , ${ eval('course_data.hole_'+i+'.cordinates.lng')}
+              `,
+        });
+
+        markers[i] = new google.maps.Marker({position: eval('course_data.hole_'+i+'.cordinates'), 
+        map: map, icon: 'http://maps.google.com/mapfiles/kml/pal2/icon13.png'});
+
+        markers[i].addListener('click', function() {
+          infowindow[i].open(map, markers[i]);
+        });
+
+
+        
+        new google.maps.Marker({position: user_loc, map: map, 
+        icon: 'http://maps.google.com/mapfiles/kml/shapes/golf.png'});
+
+    } // for (let i = 1; i < 3; i++)
+
+} // renderCourseMap()
+
+google.maps.event.addDomListener(window, 'load', renderCourseMap)
